cfbb72551efd86925052903f58efee9f1487e49f
Janelle Iglesias <45444194+Devjayz@users.noreply.github.com>
2021-10-20 21:22:31 +0800

    Eslint plugin (#5)
    * Add eslint
    
    * Fix Errors

--- src/App.js
+++ src/App.js
@@ -10,22 +10,20 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCKprJfDn8P7h7VfedQpJfW6tBbsRMGtoc",
-  authDomain: "proj2configfire.firebaseapp.com",
-  projectId: "proj2configfire",
-  storageBucket: "proj2configfire.appspot.com",
-  messagingSenderId: "11786303189",
-  appId: "1:11786303189:web:ecf587a8d174e957f3945c",
-  measurementId: "G-L1Y11JMHLB"
-})
+  apiKey: 'AIzaSyCKprJfDn8P7h7VfedQpJfW6tBbsRMGtoc',
+  authDomain: 'proj2configfire.firebaseapp.com',
+  projectId: 'proj2configfire',
+  storageBucket: 'proj2configfire.appspot.com',
+  messagingSenderId: '11786303189',
+  appId: '1:11786303189:web:ecf587a8d174e957f3945c',
+  measurementId: 'G-L1Y11JMHLB',
+});
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 const analytics = firebase.analytics();
 
-
 function App() {
-
   const [user] = useAuthState(auth);
 
   return (
@@ -44,27 +42,24 @@ function App() {
 }
 
 function SignIn() {
-
   const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider);
-  }
+  };
 
   return (
     <>
       <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
       <p>Do not violate the community guidelines or you will be banned for life!</p>
     </>
-  )
-
+  );
 }
 
 function SignOut() {
   return auth.currentUser && (
     <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
-  )
+  );
 }
-
 
 function ChatRoom() {
   const dummy = useRef();
@@ -75,7 +70,6 @@ function ChatRoom() {
 
   const [formValue, setFormValue] = useState('');
 
-
   const sendMessage = async (e) => {
     e.preventDefault();
 
@@ -85,45 +79,47 @@ function ChatRoom() {
       text: formValue,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
       uid,
-      photoURL
-    })
+      photoURL,
+    });
 
     setFormValue('');
     dummy.current.scrollIntoView({ behavior: 'smooth' });
-  }
+  };
 
-  return (<>
-    <main>
+  return (
+    <>
+      <main>
 
-      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+        {messages && messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
 
-      <span ref={dummy}></span>
+        <span ref={dummy} />
 
-    </main>
+      </main>
 
-    <form onSubmit={sendMessage}>
+      <form onSubmit={sendMessage}>
 
-      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
 
-      <button type="submit" disabled={!formValue}>🕊️</button>
+        <button type="submit" disabled={!formValue}>🕊️</button>
 
-    </form>
-  </>)
+      </form>
+    </>
+  );
 }
-
 
 function ChatMessage(props) {
   const { text, uid, photoURL } = props.message;
 
   const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
-  return (<>
-    <div className={`message ${messageClass}`}>
-      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
-      <p>{text}</p>
-    </div>
-  </>)
+  return (
+    <>
+      <div className={`message ${messageClass}`}>
+        <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} alt="" />
+        <p>{text}</p>
+      </div>
+    </>
+  );
 }
-
 
 export default App;
